f4d267b0d8ad40235cf03707eb3b20dbeaf119c5
Anurag Kashyap <swiftycodes@gmail.com>
2023-05-23 13:10:18 +0530

    Search model class created

---
+++ Financial App with Stock/Model/SearchResultsModel.swift
@@ -0,0 +1,33 @@
+//
+//  SearchResultsModel.swift
+//  FinancialAppWithStock
+//
+//  Created by Anudhi on 23/05/23.
+//
+
+import Foundation
+
+struct SearchResultsModel : Decodable {
+    
+    let items : [SearchResult]
+    
+    enum CodingKeys: String, CodingKey {
+        case items = "bestMatches"
+    }
+}
+
+
+struct SearchResult : Decodable {
+    
+    let symbol : String
+    let name : String
+    let type : String
+    let currency : String
+    
+    enum CodingKeys: String , CodingKey {
+        case symbol = "1. symbol"
+        case name = "2. name"
+        case type = "3. type"
+        case currency = "8. currency"
+    }
+}
---
+++ Financial App with Stock/Services/APIServices.swift
@@ -0,0 +1,13 @@
+//
+//  Services.swift
+//  FinancialAppWithStock
+//
+//  Created by Anudhi on 23/05/23.
+//
+
+import Foundation
+
+struct APIServices {
+    
+    let API_Key = "m7Y5SgiCgOIhcTwjqiGf9UC_GEBbNuO9"
+}
